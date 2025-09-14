8f4a0cbca3841db03e4c41ccf434c49d8c599dfd
sumits23233@gmail.com <sumits23233@gmail.com>
2025-09-14 08:58:47 +0530

    cutted and pasted code of notification bar to function because coz was getting lasrger and we was not able to make track of code

--- scripts/first.js
+++ scripts/first.js
@@ -2,6 +2,42 @@ import {cart} from '../data/cart.js';
 import {products} from '../data/products.js'
 
 let productsHTML='';
+function addToCart(productId){
+   let matchingItem;
+        cart.forEach((item)=>{
+          if(productId===item.productId){
+            matchingItem=item;
+          }
+        });
+
+          if(matchingItem){
+            matchingItem.quantity+=1;
+          }
+          else{
+             cart.push({
+          productId:productId,
+          quantity:1
+        });
+          }
+
+
+          let cartQuantity = 0;
+          cart.forEach((item)=>{
+            cartQuantity+=item.quantity;
+
+          });
+          
+          document.getElementById('cart-quantity')
+          .innerHTML=cartQuantity
+
+
+}
+
+
+
+
+
+
 
 
 
@@ -76,35 +112,9 @@ products.forEach((products)=>{
     .forEach((button)=>{
       button.addEventListener('click',()=>{
         const productId=button.dataset.productsId;
-        let matchingItem;
-        cart.forEach((item)=>{
-          if(productId===item.productId){
-            matchingItem=item;
-          }
-        });
-
-          if(matchingItem){
-            matchingItem.quantity+=1;
-          }
-          else{
-             cart.push({
-          productId:productId,
-          quantity:1
-        });
-          }
-
-
-          let cartQuantity = 0;
-          cart.forEach((item)=>{
-            cartQuantity+=item.quantity;
-
-          });
+       addToCart(productId);
           
-          document.getElementById('cart-quantity')
-          .innerHTML=cartQuantity
-
-          
-        console.log(cart); 
+         
 
         
 
